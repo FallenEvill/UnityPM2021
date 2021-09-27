5d22282073dc11570f4af3c28c61531b164571da
FallenEvill <50545892+FallenEvill@users.noreply.github.com>
2021-09-27 21:43:15 +0500

    Pract2

---
+++ Pendulum2021/Assets/Scripts/RopePendulum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopePendulum : MonoBehaviour
+{
+    public float angle = 0.0f;
+    public float l = 5f;
+    public float g = 9.8f;
+    public float anglSpd = 0f;
+    private float alpha;
+    private float omega;
+    private float amp;
+    private float move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        omega = Convert.ToSingle(Math.Sqrt(g / l));
+        angle = Mathf.Deg2Rad * angle;
+        if (anglSpd > 0)
+            alpha = Mathf.Atan(angle * omega / anglSpd);
+        else
+            alpha = angle;
+        amp = Mathf.Sqrt(Mathf.Sqrt(angle) + (Mathf.Sqrt(anglSpd) / Mathf.Sqrt(omega)));
+        transform.position = new Vector3(l*Mathf.Sin(angle), -l*Mathf.Cos(alpha), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Mathf.Sin(Time.time * omega + alpha) * amp;
+        transform.position = new Vector3(Mathf.Sin(move)*l, -Mathf.Cos(move)*l, 0);
+
+    }
+}
---
+++ Pendulum2021/Assets/Scripts/SpringPendulum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringPendulum : MonoBehaviour
+{
+    private int count = 0;
+    public float omega;
+    public float amp, alpha;
+    public float k = 0.4f;
+    public float speed = 0f;
+    public float mass = 0f;
+    public float stPlace = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        omega = Mathf.Sqrt(mass / k);
+        if (speed > 0)
+            alpha = Mathf.Atan(stPlace * k / speed);
+        else
+            alpha = stPlace;
+        amp = Mathf.Sqrt(stPlace * stPlace + (speed * speed / k * k));
+        transform.position = new Vector3(Mathf.Sin(alpha) * amp, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Convert.ToSingle(Math.Sin(Time.time * omega + alpha)) * amp, 0, 0);
+
+    }
+}
